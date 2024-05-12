69f1f35559d17fab36621921f0e8361a99f3e653
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-05-12 22:41:02 +0900

    route order

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,9 @@
 package com.example.plugins
 
 import com.example.routes.customerRouting
+import com.example.routes.getOrderRoute
+import com.example.routes.listOrdersRoute
+import com.example.routes.totalizeOrderRoute
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,6 +11,9 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
 
